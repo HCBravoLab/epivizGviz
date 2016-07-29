4fb612b6f0abb47db996e07d1a83db660708b373
Morgan Walter <Morgan Walter>
2016-07-29 16:09:35 -0400

    update vignette for gviz to epiviz functions

--- R/convertGvizGeneRegion.R
+++ R/convertGvizGeneRegion.R
@@ -1,6 +1,6 @@
 # converts Gviz GeneRegionTrack to Epiviz GenesTrack
 
-convertGvizGeneRegion <- function(track, annotation=NULL) {
+convertGvizGeneRegion <- function(track, annotation=NULL, chr=NULL, start=NULL, end=NULL) {
 
   # check arguments
   if (!is(track, "GeneRegionTrack")) {
@@ -15,8 +15,14 @@ convertGvizGeneRegion <- function(track, annotation=NULL) {
   }
 
   # create epiviz chart
-  chr <- track@chromosome
-  start <- min(start(track))
-  end <- max(end(track))
+  if (is.null(chr)) {
+    chr <- track@chromosome
+  }
+  if (is.null(start)) {
+    start <- min(start(track))
+  }
+  if (is.null(end)) {
+    end <- max(end(track))
+  }
   app <- startStandalone(annotation, chr=chr, start=start, end=end, port=7356)
 }
--- R/gvizToEpiviz.R
+++ R/gvizToEpiviz.R
@@ -1,6 +1,6 @@
 # convert an existing Gviz plot into an Epiviz workspace
 
-gvizToEpiviz <- function(gviz_plot) {
+gvizToEpiviz <- function(gviz_plot, annotation=NULL, chr=NULL, start=NULL, end=NULL) {
 
   # check arguments
   if (!is(gviz_plot, "list")) {
@@ -8,10 +8,17 @@ gvizToEpiviz <- function(gviz_plot) {
   }
 
   # create epiviz workspace
+  app <- NULL
   for (track in gviz_plot) {
     if (class(track)=="GeneRegionTrack" || class(track)=="BiomartGeneRegionTrack") {
-      app <- convertGvizGeneRegion(track=track)
-    } else if (class(track)=="AnnotationTrack") {
+      app <- convertGvizGeneRegion(track=track, annotation=annotation, chr=chr, start=start, end=end)
+    }
+  }
+  if (is.null(app)) {
+    app <- startStandalone(annotation, chr=chr, start=start, end=end, port=7356)
+  }
+  for (track in gviz_plot) {
+    if (class(track)=="AnnotationTrack") {
       convertGvizAnnotation(app=app, track=track)
     } else if (class(track)=="DataTrack") {
       convertGvizData(app=app, track=track)
